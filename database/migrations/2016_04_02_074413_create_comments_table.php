9da8ea84cc71c05433271b4f83d8aa18278994a9
Cassiszuoan <jerry831210@gmail.com>
2016-04-02 15:54:30 +0800

    basic migration done

---
+++ database/migrations/2016_04_02_074413_create_comments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 255);
+            $table->string('email',255);
+            $table->text('content');
+            $table->integer('post_id');
+            $table->timestamps('added_on');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('comments');
+    }
+}
---
+++ resources/views/layouts/bladeexample.blade.php
@@ -0,0 +1,5 @@
+@extends ('layouts.master')
+@section('title','Page Title')
+@section('content')
+<p>This is my body content.</p>
+@endsection
